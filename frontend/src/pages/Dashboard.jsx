78dfc0160b43babd43cd18430a619fbe617c60ef
LuisVillela <luisgmorales@ufm.edu>
2025-11-20 21:00:13 -0800

    import lib

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import LayoutResponsive from "../layout/LayoutResponsive";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { FiRefreshCw } from "react-icons/fi";
+
 
 export default function Dashboard() {
   const { user, refreshBalance } = useContext(AuthContext);
